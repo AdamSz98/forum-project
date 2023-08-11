5ccb8b14a4f1353c80ef690a599e1f86fefeadb2
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-08-11 21:58:28 +0200

    FRONTEND: Add searchbar to navbar

--- forum-app/app/components/navbar/Navbar.tsx
+++ forum-app/app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 import { RiAliensFill } from 'react-icons/ri';
+import { BsSearch } from 'react-icons/bs';
 
 const Navbar = () => {
   return (
@@ -11,6 +12,16 @@ const Navbar = () => {
         </div>
         <h3 className={styles.title}>forum</h3>
       </Link>
+      <div className={styles.searchBar}>
+        <label className={styles.searchIcon}>
+          <BsSearch />
+        </label>
+        <input 
+          type="text" 
+          placeholder="Search in our forum"
+          className={styles.searchInput}
+        />
+      </div>
     </nav>
   )
 }
