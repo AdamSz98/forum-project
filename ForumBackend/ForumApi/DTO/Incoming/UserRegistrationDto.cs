9855bdb91ad37acd2c9adf7cc4edd7e227d1fb36
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-08-13 11:18:29 +0200

    BACKEND: Download Automapper package, Create UserRegistrationDto

---
+++ ForumBackend/ForumApi/DTO/Incoming/UserRegistrationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ForumApi.DTO.Incoming
+{
+    public class UserRegisterDto
+    {
+        [JsonPropertyName("username")]
+        [StringLength(maximumLength: 64, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+        [JsonPropertyName("email")]
+        [StringLength(maximumLength: 128, MinimumLength = 3)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [JsonPropertyName("password")]
+        [StringLength(maximumLength: 30, MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- ForumBackend/ForumApi/GlobalUsing.cs
+++ ForumBackend/ForumApi/GlobalUsing.cs
@@ -1,11 +1,14 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using System.Security.Cryptography;
 global using System.Text;
+global using System.Text.Json.Serialization;
 global using System.Text.RegularExpressions;
 
 global using Microsoft.EntityFrameworkCore;
+global using AutoMapper;
 
 global using ForumApi.Data;
+global using ForumApi.DTO.Incoming;
 global using ForumApi.Helpers;
 global using ForumApi.Models;
 global using ForumApi.Repositories;
